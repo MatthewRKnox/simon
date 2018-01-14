1a261bcbeabfcc68dbed73a2c1e36e42ff942534
Matthew Knox <mattknox@gmail.com>
2018-01-14 13:01:43 +0700

    Project is fully functional if ugly.

--- simon.js
+++ simon.js
@@ -1,60 +1,55 @@
 // Before first Clean 225 Lines of codes
-var soundPlaying = false;
-var gameInProgress = false;
 var waitYourTurn = true;
-var sequenceLength = 1;
 var simonSaid = [];
 var nextColor = 0;
 var colorLit = false;
 var strict = false;
+
+var title = document.getElementById("title");
+
 var strictButton = document.getElementById("strict");
 strictButton.onclick = function() {
   if (strict) {
-    strict = false;
     strictButton.innerHTML = "Casual";
     this.style.color = "#19A54F";
   } else if (!strict) {
-    strict = true;
     strictButton.innerHTML = "Strict";
     this.style.color = "#e50b21";
   }
+  strict = !strict;
 };
+
 var startButton = document.getElementById("start");
+startButton.addEventListener("click", function() {
+  if (startButton.innerHTML == "Start") {
+    startButton.innerHTML = "Watch Carefullly...";
+    simonsPlan();
+  }
+});
+
+var restartButton = document.getElementById("restart");
+restartButton.addEventListener("click", function() {
+  restart();
+});
+function restart() {
+  window.location.href = window.location.href;
+}
+
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var greenSound = new Sound(440, "triangle");
 var redSound = new Sound(523.3, "triangle");
 var blueSound = new Sound(587.3, "triangle");
 var yellowSound = new Sound(659.3, "triangle");
 
-startButton.addEventListener("click", function() {
-  if (!gameInProgress) {
-    startButton.innerHTML = "Watch Carefullly...";
-    simonsPlan();
-  }
-  gameInProgress = true;
-});
-var title = document.getElementById("title");
-
-var restartButton = document.getElementById("restart");
-restartButton.addEventListener("click", function() {
-  restart();
-});
-
-function restart() {
-  window.location.href = window.location.href;
-}
-
 function Sound(frequency) {
-  this.osc = audioCtx.createOscillator(); // Create oscillator node
+  this.osc = audioCtx.createOscillator();
   this.osc.frequency.value = frequency;
   this.osc.type = "triangle";
   this.osc.start(0);
 }
-
 Sound.prototype.play = function() {
   this.osc.connect(audioCtx.destination);
 };
-
 Sound.prototype.stop = function() {
   this.osc.disconnect();
 };
@@ -83,45 +78,39 @@ var blue = {
   lit: "#298FFB",
   sound: blueSound
 };
-var colors = [green, red, blue, yellow];
 
-green.element.addEventListener("click", function() {
-  if (!waitYourTurn) {
-    yourTurn(green);
+var colors = [green, red, yellow, blue];
+colorClicked(red);
+colorClicked(red);
+colorClicked(blue);
+colorClicked(yellow);
+
+function colorClicked(color){
+   color.element.addEventListener("click", function() {
+      if (!waitYourTurn) {
+        yourTurn(color);
+      }
+    });
   }
-});
-red.element.addEventListener("click", function() {
-  if (!waitYourTurn) {
-    yourTurn(red);
-  }
-});
-yellow.element.addEventListener("click", function() {
-  if (!waitYourTurn) {
-    yourTurn(yellow);
-  }
-});
-blue.element.addEventListener("click", function() {
-  if (!waitYourTurn) {
-    yourTurn(blue);
-  }
-});
 
 function light(color, toneLength) {
   this.toneLength = toneLength;
   color.element.style.backgroundColor = color.lit;
   playSound(color.sound, this.toneLength);
 }
-function playSound(colorSound, toneLength, start) {
-  this.colorSound = colorSound;
+function playSound(colorSound, toneLength, start, isPlaying) {
+  var isPlaying = isPlaying || false;
   var start = start || 0;
-  soundPlaying ? colorSound.stop() : colorSound.play();
-  soundPlaying ? (soundPlaying = false) : (soundPlaying = true);
+  this.colorSound = colorSound;
+  isPlaying ? colorSound.stop() : colorSound.play();
+  isPlaying = !isPlaying;
   if (start < 1) {
     setTimeout(function() {
-      playSound(this.colorSound, toneLength, start + 1);
+      playSound(this.colorSound, toneLength, start + 1, isPlaying);
     }, toneLength);
   }
 }
+
 function unlight(color) {
   color.element.style.backgroundColor = color.unlit;
 }
@@ -131,14 +120,14 @@ function repeatAfterMe(simonSaid, start) {
     simonColor = start;
     if (!colorLit) {
       light(simonSaid[simonColor], 800);
-      colorLit = true;
     } else {
       unlight(simonSaid[simonColor]);
-      colorLit = false;
     }
-    if (start < simonSaid.length - 1) {
+    colorLit = !colorLit;
+    start = colorLit ? start : start + 1;
+    if (start < simonSaid.length) {
       setTimeout(function() {
-        repeatAfterMe(simonSaid, start + 1);
+      repeatAfterMe(simonSaid, start);
       }, 1000);
     } else {
       waitYourTurn = false;
@@ -150,16 +139,15 @@ function simonsPlan() {
   var random = Math.floor(Math.random() * 4);
   simonsColor = colors[random];
   simonSaid.push(simonsColor);
-  simonSaid.push(simonsColor);
-  title.innerHTML = "Steps: " + simonSaid.length / 2;
+  title.innerHTML = "Steps: " + simonSaid.length;
   repeatAfterMe(simonSaid);
 }
 
 function yourTurn(yourGuess) {
   if (yourGuess == simonSaid[nextColor]) {
     light(yourGuess, 200);
-    nextColor = nextColor + 2;
-    if (nextColor == 40) {
+    nextColor++;
+    if (nextColor == 20) {
       youWin();
       return;
     }
@@ -179,7 +167,10 @@ function yourTurn(yourGuess) {
       }
       else {
         youFailed();
-        repeatAfterMe(simonSaid);
+        setTimeout(function() {
+          repeatAfterMe(simonSaid);
+        }, 2000);
+
       }
   }
 }
@@ -190,7 +181,7 @@ function youFailed(start) {
   if (start < 1) {
     setTimeout(function() {
       youFailed(start + 1);
-    }, 1000);
+    }, 2000);
   } else {
     startButton.innerHTML = "Watch Carefullly...";
   }
